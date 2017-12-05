9b481eeed339f78cf2135f4140f5e95e0fd550ba
Scorpio79 <pshyk.oleg@gmail.com>
2017-12-05 11:41:12 +0200

    Fix vice versa

--- viceversa/js/app.js
+++ viceversa/js/app.js
@@ -1,4 +1,7 @@
 
+/*=======================================================*/
+// BUTTONS CLICK
+/*=======================================================*/
 // add class mobile-menu-dropped to #header   (small screen)
 function dropped() {
 	document.querySelector("#header").classList.toggle("mobile-menu-dropped");    			
@@ -8,16 +11,83 @@ function dropdown(){
 	var el = document.querySelector("#header");
 	el.classList.toggle("large-dropdown"); 
 }
-
-
 // for mobile button "menu-button"
-document.querySelector(".menu-button").addEventListener("click", dropped)
+document.querySelector(".menu-button").addEventListener("click", dropped);
 
 // for mobile button "close-button"
-document.querySelector(".close-button").addEventListener("click", dropped)
+document.querySelector(".close-button").addEventListener("click", dropped);
 
+/*=======================================================*/
+// HOVER mouseover & mouseout
+/*=======================================================*/
+// Vice Versa Art Books
+var logo = document.querySelector(".logo"),
+	hoverArt = document.querySelector(".hover-art"),
+	testHeader = document.querySelector("#header");
+			
+// check if mobile menu	dropped?
+function isMobileDrop(el) {
+  	return ( el.classList.contains('mobile-menu-dropped') ? true : false );
+}
+function isLargeDrop(el) {
+  	return ( el.classList.contains('large-dropdown') ? true : false );
+}
 
+function addArt() {	
+ 	if(hoverArt !== null){
+		if ( (!hoverArt.classList.contains("on")) && (!isMobileDrop(testHeader)) && (!isLargeDrop(testHeader)) ){
+			//hoverArt.classList.add("on"); 
+			setTimeout(function(){hoverArt.classList.add("on");}, 200);		   			
+		} 
+	} 			
+}
+
+function removeArt() {
+	if(hoverArt !== null){
+		if (hoverArt.classList.contains("on")){
+			//hoverArt.classList.remove("on");
+			setTimeout(function(){hoverArt.classList.remove("on");}, 500);		   			
+		}
+	}
+}
+if(logo !== null){
+	logo.addEventListener("mouseover", addArt);
+	logo.addEventListener("mouseout", removeArt);
+}
+
+/*=======================================================*/
+// LOGINNED mouseover & mouseout
+/*=======================================================*/
+// LOGINNED MAIL DISPLAY
+var loginned = document.querySelector(".loginned"),
+	hoverLoginned = document.querySelector(".hover-loginned");
+	// testHeader = document.querySelector("#header"); //already is ready before
+
+function addLoginned() {	
+ 	if(hoverLoginned !== null){
+		if ( (!hoverLoginned.classList.contains("on")) && (!isMobileDrop(testHeader)) && (!isLargeDrop(testHeader)) ){
+			//hoverLoginned.classList.add("on"); 
+			setTimeout(function(){hoverLoginned.classList.add("on");}, 200);		   			
+		} 
+	} 			
+}
+
+function removeLoginned() {
+	if(hoverLoginned !== null){
+		if (hoverLoginned.classList.contains("on")){
+			//hoverLoginned.classList.remove("on");
+			setTimeout(function(){hoverLoginned.classList.remove("on");}, 500);		   			
+		}
+	}
+}
+if(loginned !== null){
+	loginned.addEventListener("mouseover", addLoginned);
+	loginned.addEventListener("mouseout", removeLoginned);
+}
+
+/*=======================================================*/
 // FOOTER-REVEAL
+/*=======================================================*/
 $('footer').footerReveal();
 
 
@@ -47,4 +117,4 @@ else {
 							   mobile: true});
 	sr.reveal('.reveal-v1');
 }
-
+/*=======================================================*/

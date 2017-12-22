df07892bce4ecf699077eab471561f2fd5593e42
Scorpio79 <pshyk.oleg@gmail.com>
2017-12-22 02:15:36 +0200

    Fix smart

--- smart-fest/js/app.js
+++ smart-fest/js/app.js
@@ -10,13 +10,26 @@ var contactSubmit = document.querySelector(".submit-send"),
     contactEmail = document.querySelector("#contact-email"),
     contactText = document.querySelector("#contact-text");
 if (contactSubmit !== null) {
-	contactSubmit.addEventListener("click", { handleEvent: checkSubmitContactForm });
+	contactSubmit.addEventListener("click", handlerSubmitContactForm);
+	contactName.addEventListener("keydown", handlerEnter);
+	contactEmail.addEventListener("keydown", handlerEnter);
 }
-function checkSubmitContactForm(e) {
+function handlerSubmitContactForm(e) {
 	e.preventDefault();
+	checkSubmitContactForm();
+}
+//keydown Enter key in input form 
+function handlerEnter(e) {
+	e = e || window.event;
+	if (e.keyCode === 13) {
+		console.log("Enter key pressed.");
+		e.preventDefault();
+		checkSubmitContactForm();
+	}
+}
+function checkSubmitContactForm() {
 	var check = true,
 	    focus = true;
-
 	if (contactName.value == "") {
 		contactName.classList.add("invalid");
 		check = false;
@@ -53,6 +66,9 @@ function isValidEmailAddress(email) {
 function submitSuccess() {
 	if (contactSuccess !== null && !contactSuccess.classList.contains("ok")) {
 		contactSuccess.classList.add("ok");
+		contactSubmit.removeEventListener("click", handlerSubmitContactForm);
+		contactName.removeEventListener("keydown", handlerEnter);
+		contactEmail.removeEventListener("keydown", handlerEnter);
 	}
 	//contactForm.submit();	   //???  Without reloading the page
 }
@@ -71,40 +87,66 @@ var getNotified = document.querySelectorAll(".notified-btn"),
 
 if (getNotified.lenght !== 0) {
 	getNotified.forEach(function (el) {
-		return el.addEventListener("click", { handleEvent: openNotifiedModal1 });
+		return el.addEventListener("click", openNotifiedModal1);
 	});
 }
-function openNotifiedModal1(e) {
-	e.preventDefault();
-	if (getNotifiedModal1 !== null && !getNotifiedModal1.classList.contains("on")) getNotifiedModal1.classList.add("on");
+function openNotifiedModal1() {
+	if (getNotifiedModal1 !== null && !getNotifiedModal1.classList.contains("on")) {
+		getNotifiedModal1.classList.add("on");
+		submitNotified.focus();
+		document.addEventListener("keydown", handlerEscModal1);
+		cancelModal1Button.addEventListener("click", handlerCloseNotifiedModal1);
+		notifiedName.addEventListener("keydown", handlerEnterModal);
+		notifiedEmail.addEventListener("keydown", handlerEnterModal);
+	}
 }
 /*=============================================================================*/
 //					CANCEL MODAL(SMALL) CONTACT FORM
 /*=============================================================================*/
-if (cancelModal1Button !== null) {
-	cancelModal1Button.addEventListener("click", { handleEvent: closeNotifiedModal1 });
-}
-function closeNotifiedModal1(e) {
+function handlerCloseNotifiedModal1(e) {
 	e.preventDefault();
-	if (getNotifiedModal1 !== null && getNotifiedModal1.classList.contains("on")) getNotifiedModal1.classList.remove("on");
-	resetInvalid();
+	getNotifiedModal1.removeEventListener("keydown", handlerEscModal1);
+	closeNotifiedModal1();
+}
+function closeNotifiedModal1() {
+	if (getNotifiedModal1 !== null && getNotifiedModal1.classList.contains("on")) {
+		document.removeEventListener("keydown", handlerEscModal1);
+		notifiedName.removeEventListener("keydown", handlerEnterModal);
+		notifiedEmail.removeEventListener("keydown", handlerEnterModal);
+		getNotifiedModal1.classList.remove("on");
+		resetInvalid();
+	}
+}
+// key  ESC in getNotifiedModal1 pressed
+function handlerEscModal1(e) {
+	e = e || window.event;
+	if (e.keyCode == 27) {
+		//console.log("Esc key pressed.");
+		e.preventDefault();
+		closeNotifiedModal1();
+	}
 }
 /*=============================================================================*/
 //					MODAL(SMALL) CONTACT FORM SUBMIT WITH JS VALIDATION
 /*=============================================================================*/
 
 if (submitNotified !== null) {
-	submitNotified.addEventListener("click", { handleEvent: checkSubmitFormModal });
+	submitNotified.addEventListener("click", handlerCheckSubmitFormModal);
 }
-//keydown Enter key in form
-formModal.addEventListener("keydown", function (e) {
-	if (event.keyCode === 13) {
+//keydown Enter key in input form 
+function handlerEnterModal(e) {
+	e = e || window.event;
+	if (e.keyCode === 13) {
+		//console.log("Enter key pressed.");
 		e.preventDefault();
-		checkSubmitFormModal(e);
+		checkSubmitFormModal();
 	}
-});
-function checkSubmitFormModal(e) {
+}
+function handlerCheckSubmitFormModal(e) {
 	e.preventDefault();
+	checkSubmitFormModal();
+}
+function checkSubmitFormModal() {
 	var checkM = true,
 	    focusM = true;
 	if (notifiedName.value == "") {
@@ -126,7 +168,7 @@ function checkSubmitFormModal(e) {
 	} else if (notifiedEmail.classList.contains("invalid")) notifiedEmail.classList.remove("invalid");
 
 	if (checkM) {
-		openNotifiedModal2(e);
+		openNotifiedModal2();
 		//formModal.submit();	   //???  Without reloading the page
 	}
 }
@@ -134,22 +176,47 @@ function resetInvalid() {
 	if (notifiedName.classList.contains("invalid")) notifiedName.classList.remove("invalid");
 	if (notifiedEmail.classList.contains("invalid")) notifiedEmail.classList.remove("invalid");
 }
-function openNotifiedModal2(e) {
-	if (getNotifiedModal2 !== null && !getNotifiedModal2.classList.contains("on")) getNotifiedModal2.classList.add("on");
-	closeNotifiedModal1(e);
-	document.addEventListener("click", function (event) {
-		var target = findClosest(event.target, function (el) {
-			return el.id == "getNotified2";
-		});
-		if (!target) {
-			closeNotifiedModal2();
-		}
-	}, true);
+function openNotifiedModal2() {
+	if (getNotifiedModal2 !== null && !getNotifiedModal2.classList.contains("on")) {
+		getNotifiedModal2.classList.add("on");
+		closeNotifiedModal1();
+		getNotifiedModal2.addEventListener("click", handlerClickOut);
+		document.addEventListener("keydown", handlerEscModal2);
+	}
+}
+// key  ESC in getNotifiedModal2 pressed
+function handlerEscModal2(e) {
+	e = e || window.event;
+	if (e.keyCode == 27) {
+		//console.log("Esc key pressed.");
+		e.preventDefault();
+		resetValue();
+		closeNotifiedModal2();
+	}
+}
+function handlerClickOut(e) {
+	var target = findClosest(e.target, function (el) {
+		//console.log("click IN!!!");
+		return el.id == "getNotified2";
+	});
+	if (!target) {
+		//console.log("click OUT!!!");
+		resetValue();
+		closeNotifiedModal2();
+	}
 }
 function findClosest(element, fn) {
 	if (!element) return undefined;
 	return fn(element) ? element : findClosest(element.parentElement, fn);
 }
 function closeNotifiedModal2() {
-	if (getNotifiedModal2 !== null && getNotifiedModal2.classList.contains("on")) getNotifiedModal2.classList.remove("on");
+	if (getNotifiedModal2 !== null && getNotifiedModal2.classList.contains("on")) {
+		getNotifiedModal2.removeEventListener("click", handlerClickOut);
+		document.removeEventListener("keydown", handlerEscModal2);
+		getNotifiedModal2.classList.remove("on");
+	}
+}
+function resetValue() {
+	notifiedName.value = "";
+	notifiedEmail.value = "";
 }

5faa55ee357b97aaf1e3393e2acf24fabbdef9b7
Scorpio79 <pshyk.oleg@gmail.com>
2018-01-05 06:16:11 +0200

    sdfgsdfg

--- oopopopo/js/app.js
+++
@@ -1,1 +0,0 @@
-window.onload=function(){Typed.new(".typed-strings",{stringsElement:document.getElementById('typed-strings'),typeSpeed:20,backDelay:2000,loop:true});};window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','UA-2003587-5');
---
+++ zzz/js/app.js
@@ -0,0 +1,1 @@
+window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','UA-2003587-5');

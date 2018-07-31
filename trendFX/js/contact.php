6c355ef9923d3cc0cdbc59e24db57cd59ff986e3
OlehPshyk <pshyk.oleg@gmail.com>
2018-07-31 20:14:12 +0300

    landing trandFX

---
+++ trendFX/js/contact.php
@@ -0,0 +1,8 @@
+<?php
+    // $name = $_POST['name'];
+    // $phone = $_POST['phone'];
+    // $email = $_POST['email']; 
+    echo("Name: " . $_POST['name'] . "<br />\n");
+   	echo("Phone: " . $_POST['phone'] . "<br />\n");
+   	echo("Email: " . $_POST['email'] . "<br />\n");   
+?>

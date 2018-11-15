3dc0c7095771933bda8ba3146f37afe378a1b180
Oleh Pshyk <pshyk.oleg@gmail.com>
2018-11-15 15:34:48 +0200

    dd

--- trendFXLanding/js/app.js
+++ trendFXLanding/js/app.js
@@ -1,171 +1,191 @@
 "use strict";
 
-// capcha initialize
-var captchaImageCode = document.getElementById("CaptchaImageCode"),
-    userCaptchaCode = document.getElementById("UserCaptchaCode"),
-    wrongCaptchaError = document.getElementById("WrongCaptchaError"),
-    successMessage = document.getElementById("SuccessMessage"),
-    form = document.getElementById("form"),
-    reloadBtn = document.getElementById("ReloadBtn");
-var cd;
-CreateCaptcha();
-form && form.addEventListener("submit", checkForm);
-reloadBtn && reloadBtn.addEventListener("click", CreateCaptcha);
+document.addEventListener('DOMContentLoaded', function () {
 
-function checkForm(e) {
-	ValidateCaptcha() ? goodCapcha(e) : badCapcha(e);
-	return false;
-}
-function goodCapcha(e) {
-	//console.log("submit");
-	//e.preventDefault();
+  // capcha initialize
+  var captchaImageCode = document.getElementById("CaptchaImageCode"),
+      userCaptchaCode = document.getElementById("UserCaptchaCode"),
+      wrongCaptchaError = document.getElementById("WrongCaptchaError"),
+      successMessage = document.getElementById("SuccessMessage"),
+      form = document.getElementById("form"),
+      reloadBtn = document.getElementById("ReloadBtn");
+  var cd;
+  CreateCaptcha();
+  form && form.addEventListener("submit", checkForm);
+  reloadBtn && reloadBtn.addEventListener("click", CreateCaptcha);
 
-	if (userCaptchaCode) {
-		userCaptchaCode.value = "";
-		userCaptchaCode.setAttribute("placeholder", "Kodu giriniz");
-	}
+  function checkForm(e) {
+    ValidateCaptcha() ? goodCapcha(e) : badCapcha(e);
+    return false;
+  }
+  function goodCapcha(e) {
+    //console.log("submit");
+    //e.preventDefault();
 
-	CreateCaptcha();
-	if (wrongCaptchaError) {
-		wrongCaptchaError.style.display = "none";
-	}
-	if (successMessage) {
-		successMessage.style.display = "block";
-	}
-	submitForm(e);
-}
-function submitForm(e) {
-	// var data = new FormData(form);
+    if (userCaptchaCode) {
+      userCaptchaCode.value = "";
+      userCaptchaCode.setAttribute("placeholder", "Kodu giriniz");
+    }
 
-	// var request = new XMLHttpRequest();
+    CreateCaptcha();
+    if (wrongCaptchaError) {
+      wrongCaptchaError.style.display = "none";
+    }
+    if (successMessage) {
+      successMessage.style.display = "block";
+    }
+    submitForm(e);
+  }
+  function submitForm(e) {
+    // var data = new FormData(form);
 
-	// request.onreadystatechange = function(){
-	//   document.getElementById("result").innerText = request.responseText;
-	// }
+    // var request = new XMLHttpRequest();
 
-	// request.open(form.method, form.action);
-	// request.send(data);
+    // request.onreadystatechange = function(){
+    //   document.getElementById("result").innerText = request.responseText;
+    // }
 
-	//form && form.submit();
-	e.preventDefault();
-	e.stopPropagation();
+    // request.open(form.method, form.action);
+    // request.send(data);
 
-	var name = encodeURIComponent(document.getElementById('name').value);
-	var phone = encodeURIComponent(document.getElementById('phone').value);
-	var email = encodeURIComponent(document.getElementById('email').value);
+    //form && form.submit();
+    e.preventDefault();
+    e.stopPropagation();
 
-	var parameters = 'name=' + name + '&phone=' + phone + '&email=' + email;
+    var name = encodeURIComponent(document.getElementById('name').value);
+    var phone = encodeURIComponent(document.getElementById('phone').value);
+    var email = encodeURIComponent(document.getElementById('email').value);
 
-	var xhr = new XMLHttpRequest();
+    var parameters = 'name=' + name + '&phone=' + phone + '&email=' + email;
 
-	xhr.onreadystatechange = function (e) {
-		if (xhr.readyState === 4 && xhr.status === 200) {
-			form.innerHTML = xhr.responseText;
-		}
-	};
+    var xhr = new XMLHttpRequest();
 
-	xhr.open(form.method, form.action);
-	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xhr.send(parameters);
+    xhr.onreadystatechange = function (e) {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        form.innerHTML = xhr.responseText;
+      }
+    };
 
-	//form && form.submit();
-}
-function badCapcha(e) {
-	e.preventDefault();
-	if (userCaptchaCode && userCaptchaCode.value == "" || userCaptchaCode.value == null || userCaptchaCode.value == "undefined") {
-		if (wrongCaptchaError) {
-			wrongCaptchaError.innerHTML = "Please enter code given below in a picture.";
-			wrongCaptchaError.style.display = 'block';
-		}
-		userCaptchaCode && userCaptchaCode.focus();
-	} else {
-		if (wrongCaptchaError) {
-			wrongCaptchaError.innerHTML = "Invalid Captcha! Please try again.";
-			wrongCaptchaError.style.display = 'block';
-		}
-		CreateCaptcha();
-		userCaptchaCode && userCaptchaCode.focus();
-	}
-}
-// Create Captcha
-function CreateCaptcha() {
-	//var alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
-	//var alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', '1', '2', '3', '4', '5', '6', '7', '8', '9');
-	var alpha = new Array('1', '2', '3', '4', '5', '6', '7', '8', '9');
-	var i;
-	for (i = 0; i < 6; i++) {
-		var a = alpha[Math.floor(Math.random() * alpha.length)];
-		var b = alpha[Math.floor(Math.random() * alpha.length)];
-		var c = alpha[Math.floor(Math.random() * alpha.length)];
-		var d = alpha[Math.floor(Math.random() * alpha.length)];
-		var e = alpha[Math.floor(Math.random() * alpha.length)];
-		var f = alpha[Math.floor(Math.random() * alpha.length)];
-	}
-	cd = a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f;
-	if (captchaImageCode) {
-		while (captchaImageCode.firstChild) {
-			captchaImageCode.removeChild(captchaImageCode.firstChild);
-		}
-	}
-	//.append('<canvas id="CapCode" class="capcode"></canvas>')
-	var canvas = document.createElement('canvas');
-	if (canvas) {
-		canvas.id = "CapCode";
-		canvas.setAttribute("class", "capcode");
-		captchaImageCode && captchaImageCode.appendChild(canvas);
-		var c = document.getElementById("CapCode");
-		if (c) {
-			var ctx = c.getContext("2d"),
-			    x = c.width / 2,
-			    img = new Image();
-			img.src = "./img/captcha-bg.jpg";
-			img.onload = function () {
-				var pattern = ctx.createPattern(img, "repeat");
-				ctx.fillStyle = pattern;
-				ctx.fillRect(0, 0, c.width, c.height);
-				ctx.font = "46px Titillium Web";
-				ctx.fillStyle = '#ccc';
-				ctx.textAlign = 'center';
-				ctx.setTransform(1, -0.12, 0, 1, 0, 50);
-				ctx.fillText(cd, x, 55);
-			};
-		}
-	}
-}
-// Validate Captcha
-function ValidateCaptcha() {
-	var string1 = removeSpaces(cd);
-	var string2 = removeSpaces(userCaptchaCode.value);
-	if (string1 == string2) {
-		return true;
-	} else {
-		return false;
-	}
-}
-// Remove Spaces
-function removeSpaces(string) {
-	return string.split(' ').join('');
-}
-//Check Captcha
-// function CheckCaptcha() {	
-// 	var result = ValidateCaptcha();
-// 	if( userCaptchaCode.value == "" || userCaptchaCode.value == null || userCaptchaCode.value == "undefined") {
-// 		wrongCaptchaError.innerHTML="Please enter code given below in a picture.";
-// 		wrongCaptchaError.style.display = 'block';		
-// 	    userCaptchaCode.focus();
-// 	} else {
-// 	    if(result == false) { 
-// 	    	wrongCaptchaError.innerHTML = "Invalid Captcha! Please try again.";
-// 	    	wrongCaptchaError.style.display = 'block';
-// 	    	CreateCaptcha();
-// 	    	userCaptchaCode.focus();	    	
-// 	    }
-// 	    else { 
-// 	    	userCaptchaCode.value = "";
-// 	    	userCaptchaCode.setAttribute("placeholder", "Kodu giriniz");
-// 	    	CreateCaptcha();
-// 	    	wrongCaptchaError.style.display = "none";
-// 	    	successMessage.style.display = "block";
-// 	    }
-// 	}
-// }
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(parameters);
+
+    //form && form.submit();
+  }
+  function badCapcha(e) {
+    e.preventDefault();
+    if (userCaptchaCode && userCaptchaCode.value == "" || userCaptchaCode.value == null || userCaptchaCode.value == "undefined") {
+      if (wrongCaptchaError) {
+        wrongCaptchaError.innerHTML = "Please enter code given below in a picture.";
+        wrongCaptchaError.style.display = 'block';
+      }
+      userCaptchaCode && userCaptchaCode.focus();
+    } else {
+      if (wrongCaptchaError) {
+        wrongCaptchaError.innerHTML = "Invalid Captcha! Please try again.";
+        wrongCaptchaError.style.display = 'block';
+      }
+      CreateCaptcha();
+      userCaptchaCode && userCaptchaCode.focus();
+    }
+  }
+  // Create Captcha
+  function CreateCaptcha() {
+    //var alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+    //var alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+    var alpha = new Array('1', '2', '3', '4', '5', '6', '7', '8', '9');
+    var i;
+    for (i = 0; i < 6; i++) {
+      var a = alpha[Math.floor(Math.random() * alpha.length)];
+      var b = alpha[Math.floor(Math.random() * alpha.length)];
+      var c = alpha[Math.floor(Math.random() * alpha.length)];
+      var d = alpha[Math.floor(Math.random() * alpha.length)];
+      var e = alpha[Math.floor(Math.random() * alpha.length)];
+      var f = alpha[Math.floor(Math.random() * alpha.length)];
+    }
+    cd = a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f;
+    if (captchaImageCode) {
+      while (captchaImageCode.firstChild) {
+        captchaImageCode.removeChild(captchaImageCode.firstChild);
+      }
+    }
+    //.append('<canvas id="CapCode" class="capcode"></canvas>')
+    var canvas = document.createElement('canvas');
+    if (canvas) {
+      canvas.id = "CapCode";
+      canvas.setAttribute("class", "capcode");
+      captchaImageCode && captchaImageCode.appendChild(canvas);
+      var c = document.getElementById("CapCode");
+      if (c) {
+        var ctx = c.getContext("2d"),
+            x = c.width / 2,
+            img = new Image();
+        img.src = "./img/captcha-bg.jpg";
+        img.onload = function () {
+          var pattern = ctx.createPattern(img, "repeat");
+          ctx.fillStyle = pattern;
+          ctx.fillRect(0, 0, c.width, c.height);
+          ctx.font = "46px Titillium Web";
+          ctx.fillStyle = '#ccc';
+          ctx.textAlign = 'center';
+          ctx.setTransform(1, -0.12, 0, 1, 0, 50);
+          ctx.fillText(cd, x, 55);
+        };
+      }
+    }
+  }
+  // Validate Captcha
+  function ValidateCaptcha() {
+    var string1 = removeSpaces(cd);
+    var string2 = removeSpaces(userCaptchaCode.value);
+    if (string1 == string2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  // Remove Spaces
+  function removeSpaces(string) {
+    return string.split(' ').join('');
+  }
+  //Check Captcha
+  // function CheckCaptcha() {	
+  // 	var result = ValidateCaptcha();
+  // 	if( userCaptchaCode.value == "" || userCaptchaCode.value == null || userCaptchaCode.value == "undefined") {
+  // 		wrongCaptchaError.innerHTML="Please enter code given below in a picture.";
+  // 		wrongCaptchaError.style.display = 'block';		
+  // 	    userCaptchaCode.focus();
+  // 	} else {
+  // 	    if(result == false) { 
+  // 	    	wrongCaptchaError.innerHTML = "Invalid Captcha! Please try again.";
+  // 	    	wrongCaptchaError.style.display = 'block';
+  // 	    	CreateCaptcha();
+  // 	    	userCaptchaCode.focus();	    	
+  // 	    }
+  // 	    else { 
+  // 	    	userCaptchaCode.value = "";
+  // 	    	userCaptchaCode.setAttribute("placeholder", "Kodu giriniz");
+  // 	    	CreateCaptcha();
+  // 	    	wrongCaptchaError.style.display = "none";
+  // 	    	successMessage.style.display = "block";
+  // 	    }
+  // 	}
+  // }
+
+
+  //Testimonials Slider
+
+  $(".testimonials-slider").slick({
+
+    // normal options...
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000, //interval
+    pauseOnHover: true,
+    speed: 300 //slider-speed
+
+  });
+
+  //End Testimonials Slider
+
+});

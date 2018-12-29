9d77d2a673a5047941634192ce3e7e5d094a4b44
Oleh Pshyk <pshyk.oleg@gmail.com>
2018-12-29 19:52:40 +0200

    ee

---
+++ test-hotels/precache-manifest.8efbf4aa800e3b05edae6711f359ec09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2401827d2c7d8175a3d0",
+    "url": "/static/js/main.2401827d.chunk.js"
+  },
+  {
+    "revision": "f2362f4a1cc6adcd3ecb",
+    "url": "/static/js/1.f2362f4a.chunk.js"
+  },
+  {
+    "revision": "2401827d2c7d8175a3d0",
+    "url": "/static/css/main.dbc56a56.chunk.css"
+  },
+  {
+    "revision": "17965b430254b3ad6f02b9bb609960dd",
+    "url": "/index.html"
+  }
+];

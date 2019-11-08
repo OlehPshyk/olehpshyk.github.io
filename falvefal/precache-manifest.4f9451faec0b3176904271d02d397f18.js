ec8b0ac50e261ce8abb4e19c7e9167fea515f9f2
Scorpio79 <pshyk.oleg@gmail.com>
2019-11-09 01:05:35 +0200

    f

---
+++ falvefal/precache-manifest.4f9451faec0b3176904271d02d397f18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d22e100c14c871f796cc70514a6b627",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1bee9b79fadf23caed2a",
+    "url": "./static/css/main.9f5222a6.chunk.css"
+  },
+  {
+    "revision": "3a2cb7fcbb91c7d55355",
+    "url": "./static/js/2.c4023343.chunk.js"
+  },
+  {
+    "revision": "1bee9b79fadf23caed2a",
+    "url": "./static/js/main.411b1688.chunk.js"
+  },
+  {
+    "revision": "e36c8859b525fb6e4a1d",
+    "url": "./static/js/runtime-main.e5020d52.js"
+  }
+]);
--- falvefal/service-worker.js
+++ falvefal/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d0303bc9109f3356b6e34080f7b3c51a.js"
+  "./precache-manifest.4f9451faec0b3176904271d02d397f18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

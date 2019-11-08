6749808b66a9f21b7cf52e899c7dc4d7ab7846d7
Scorpio79 <pshyk.oleg@gmail.com>
2019-11-09 00:28:37 +0200

    f

---
+++ precache-manifest.d0303bc9109f3356b6e34080f7b3c51a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9113283666eab7246f51729e3fc77134",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bee9b79fadf23caed2a",
+    "url": "/static/css/main.9f5222a6.chunk.css"
+  },
+  {
+    "revision": "3a2cb7fcbb91c7d55355",
+    "url": "/static/js/2.c4023343.chunk.js"
+  },
+  {
+    "revision": "1bee9b79fadf23caed2a",
+    "url": "/static/js/main.411b1688.chunk.js"
+  },
+  {
+    "revision": "d7318e8bc00ee5c9d6d5",
+    "url": "/static/js/runtime-main.a36783b3.js"
+  }
+]);

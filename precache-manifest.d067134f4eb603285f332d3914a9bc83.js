9a56c112bd322df6af22460cf6a08944a0f4017e
Oleh Pshyk <pshyk.oleg@gmail.com>
2020-11-22 20:12:05 +0200

    f

---
+++ precache-manifest.d067134f4eb603285f332d3914a9bc83.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50273b47d5317ebdce73aa199d90751d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec6b7beae5af10adbbab",
+    "url": "/static/css/main.bf031472.chunk.css"
+  },
+  {
+    "revision": "0b29732de6866e22c5f6",
+    "url": "/static/js/0.3db8dd63.chunk.js"
+  },
+  {
+    "revision": "3abc725adc5a81e70c2d",
+    "url": "/static/js/5.02334439.chunk.js"
+  },
+  {
+    "revision": "6083d9c769f39b252a49c0c719a4b64c",
+    "url": "/static/js/5.02334439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6b7beae5af10adbbab",
+    "url": "/static/js/main.bb8fd87f.chunk.js"
+  },
+  {
+    "revision": "cb838f73abb06c8bc6c4",
+    "url": "/static/js/register.e323dec7.chunk.js"
+  },
+  {
+    "revision": "90c593f0d52c5572e1b1",
+    "url": "/static/js/runtime-main.45d7b7d8.js"
+  },
+  {
+    "revision": "7f93376cb846e84e2c17",
+    "url": "/static/js/thankyou.a6bb17a9.chunk.js"
+  },
+  {
+    "revision": "4aa0e24f0835843eb002a4c547ab3a2a",
+    "url": "/static/media/bg-empty.4aa0e24f.png"
+  },
+  {
+    "revision": "195fdeba66234e90690ac73e93c0ade6",
+    "url": "/static/media/down-arrow.195fdeba.svg"
+  },
+  {
+    "revision": "b31d24f71215028e6e30948870dba695",
+    "url": "/static/media/ic-mekar.b31d24f7.svg"
+  },
+  {
+    "revision": "c607ec6318330c96d52ff6fb4891dc4b",
+    "url": "/static/media/ic-register-as-borrower.c607ec63.svg"
+  },
+  {
+    "revision": "a084e3b3dfc98c2994e97bb14d0ff56e",
+    "url": "/static/media/ic-register-as-lender.a084e3b3.svg"
+  },
+  {
+    "revision": "8334901ad748c1aeb65beb6b02cee6a3",
+    "url": "/static/media/ic-thankyou-borrower.8334901a.svg"
+  },
+  {
+    "revision": "8149d1fd616468105bf41b34be162e30",
+    "url": "/static/media/ic-thankyou-default.8149d1fd.svg"
+  },
+  {
+    "revision": "f0b6e98df29b8dd1549704b4b64c2e63",
+    "url": "/static/media/ic-thankyou-register.f0b6e98d.svg"
+  },
+  {
+    "revision": "7e0deac2eff6e6b80428eea01864745e",
+    "url": "/static/media/left-arrow.7e0deac2.svg"
+  },
+  {
+    "revision": "50bfa05b918ab910be499942d5909e9a",
+    "url": "/static/media/logo.50bfa05b.svg"
+  }
+]);
